a64c1d530b6fe9afff4148321cccf62935266705
Geovanni Gonçalves <giocco@hotmail.com>
2021-06-02 11:48:58 -0300

    :package:Orçamentos

--- js/script.js
+++ js/script.js
@@ -7,3 +7,39 @@ hamburguer.addEventListener("click", function() {
     
 } );
 
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-não").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () { 
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector (" label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas` 
+    atualizarPreco()
+
+} )
+
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector ("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+    
+    
+    
+    let preco = qtde * 50;
+    if (temJs) preco *= 1.1
+    if (incluiLayout) {
+        preco = preco + 100
+    }
+    let taxaUrgencia= 1 - prazo*0.1;
+    preco = preco + (preco*taxaUrgencia);
+
+
+    
+    document.querySelector("#preco").innerHTML = "R$ " + preco.toFixed (2)
+
+}
+
